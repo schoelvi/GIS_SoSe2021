521e72f49649a9283e781449504be7d4b9e49a67
Vivien <vivien.christin.schoellkopf@hs-furtwangen.de>
2021-06-03 11:59:09 +0200

    versuch verbesserung

--- Aufgabe_3.1/script.ts
+++ Aufgabe_3.1/script.ts
@@ -1,15 +1,17 @@
 namespace Aufgabe_3_1 {
 
-    let formData: FormData = new FormData(document.forms[0]);
     
     let btLogin: HTMLButtonElement = <HTMLButtonElement>document.getElementById("login");
     btLogin.addEventListener("click", login);
 
 
     async function login(): Promise<void> {
-        let url: string = "https://gis2021vs.herokuapp.com/";
+        let formData: FormData = new FormData(document.forms[0]);
+
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
+        let url: string = "https://gis2021vs.herokuapp.com/";
+
         url = url + "?" + query.toString();
         let response: Response = await fetch(url);
         let text: string = await response.text();
